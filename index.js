30daca37386f2ff83d50ae97389a0f5b15ede2d4
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-05-20 13:44:02 +0530

    changed

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const ethers = require('ethers')
+const ethers = require('ethers');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const privateKey = process.env.PRIVATE_KEY;
 const provider = new ethers.providers.JsonRpcProvider('https://evm-testnet.nexis.network');
 
 const requestHistory = {};
 
-let gasprice= 5;
+let gasprice = 5;
 
 async function signTransaction(address) {
-  const wallet = new ethers.Wallet(privateKey, provider);
+    const wallet = new ethers.Wallet(privateKey, provider);
+    
+    // Fetch the current nonce
+    const nonce = await wallet.getTransactionCount('pending');
 
-  gasprice++;
-  const tx = {
-    to: address,
-    value: ethers.utils.parseEther('200'),
-    gasPrice: ethers.utils.parseUnits(gasprice.toString(), 'gwei'),
-    nonce:gasprice,
-  };
+    // Increment the gas price for each transaction
+    gasprice++;
+    
+    const tx = {
+        to: address,
+        value: ethers.utils.parseEther('200'),
+        gasPrice: ethers.utils.parseUnits(gasprice.toString(), 'gwei'),
+        nonce: nonce,
+        gasLimit: ethers.utils.hexlify(21000), // Fixed gas limit for standard transactions
+    };
 
-  const signedTx = await wallet.sendTransaction(tx);
-  console.log('Signed Transaction:', signedTx);
+    const signedTx = await wallet.sendTransaction(tx);
+    console.log('Signed Transaction:', signedTx);
 }
 
 app.get('/', (req, res) => {
@@ -36,19 +42,19 @@ app.get('/', (req, res) => {
 
 app.post('/faucet', async(req, res) => {
     try {
-        const {address} = req.body;
+        const { address } = req.body;
         const lastRequestTime = requestHistory[address];
         if (lastRequestTime && (Date.now() - lastRequestTime < 24 * 60 * 60 * 1000)) {
-            res.status(200).send({sent: false, error: "Address already requested tokens in the last 24 hours."});
+            res.status(200).send({ sent: false, error: "Address already requested tokens in the last 24 hours." });
             return;
         }
 
         await signTransaction(address);
         requestHistory[address] = Date.now();
-        res.status(200).send({sent: true});
+        res.status(200).send({ sent: true });
     } catch (error) {
-        console.log(error)
-        res.status(200).send({sent: false, error:"transaction queued, wait few minutes, balances will reload",error});
+        console.log(error);
+        res.status(200).send({ sent: false, error: "Transaction queued, wait a few minutes, balances will reload", error });
     }
 });
 
